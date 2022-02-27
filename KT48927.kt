1c093a6ccf9ed2021ff3c2d5c7da6709f3bb55d6
Sutaek Oh <39640447+diskerr@users.noreply.github.com>
2022-02-28 02:17:04 +0900

    Update KT48927.kt

--- KT48927.kt
+++ KT48927.kt
@@ -2,7 +2,6 @@ abstract class Base {
     private val _pair = Pair(1, 2)
         get() = field // this is *** required ***
 
-
     fun addAndPrint(d: Derived) {
         val p = (d as Base)._pair
         println("${p.first + _pair.first}, ${p.second + _pair.second}")
